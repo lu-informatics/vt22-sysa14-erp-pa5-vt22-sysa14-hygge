a12768a118402e73d2e19d2a8c60236faee14d81
ma7762la-s <majjalarsen@hotmail.com>
2022-03-01 16:23:53 +0000

    new

--- HyggeAssignment5Solution/HyggeAssignment5/DataAccessLayer.cs
+++ HyggeAssignment5Solution/HyggeAssignment5/DataAccessLayer.cs
@@ -9,70 +9,102 @@ namespace HyggeAssignment5
 {
     public class DataAccessLayer
     {
-        public static DataSet SendToDatabase(string sqlQuery){
-        
+        public static DataSet SendToDatabase(string sqlQuery, params ParamArgs[] args){
+            try
             {
-                using (SqlConnection cnn = new SqlConnection("Data Source = SYSA12; Initial Catalog = Cronos; User ID=hygge; Password = hej123;")) 
+                using (SqlConnection cnn = new SqlConnection("Data Source = SYSA12; Initial Catalog = Cronos; User ID=hygge; Password = hej123;"))
                 {
-                    
-                    string queryString = "SELECT * FROM [CRONUS Sverige AB$Employee]";
+                    cnn.Open();
+            using(SqlCommand command = new SqlCommand(sqlQuery, cnn))
+            using (SqlDataAdapter adapter = new SqlDataAdapter())
+            {
+                if (args != null)
+                        { foreach (ParamArgs param in args)
+                                command.Parameters.AddWithValue(param.ParamID, param.Value);}
+                        string queryType = sqlQuery.Substring(0, 6);
+                        List<string> wca = new List<string> { "ALL TABLES" };
+                        if (wca.Contains(queryType))
+                        {
+                            switch(queryType)
+                            {
+                                case "ALL TABLES":
+                                    adapter.showtables = command;
+                                    adapter.showtables.ExecuteNonQuery();
+                                    break;
+                            }
+                            return null;
+                        }
+                        else
 
-                    {
-                        SqlCommand command = new SqlCommand(queryString, cnn);
-                        cnn.Open();
-                        using(SqlDataReader reader = command.ExecuteReader()){
-                            while(reader.Read()){
-                                Console.WriteLine(reader.GetString(0));
 
 
+
+
+
+
+
+                        //{
+                        //    using (SqlConnection cnn = new SqlConnection("Data Source = SYSA12; Initial Catalog = Cronos; User ID=hygge; Password = hej123;")) 
+                        //    {
+
+                        //        string queryString = "SELECT * FROM [CRONUS Sverige AB$Employee]";
+
+                        //        {
+                        //            SqlCommand command = new SqlCommand(queryString, cnn);
+                        //            cnn.Open();
+                        //            using(SqlDataReader reader = command.ExecuteReader()){
+
+                        //                return(String.Format("{0}, {1}", reader[0], reader[1]));
+
+
+                        //        }
+
+                        //    void SendTable()
+                        //{
+                        //    string response = "";
+                        //    while (response != null)
+                        //    {
+                        //        response = Console.ReadLine();
+                        //        switch(response)
+                        //        {
+                        //            case "Contents and metadata for the Employee tables and related tables";
+                        //                            string queryString = "";
+                        //            break;
+                        //            case "Information about Employees and their relatives";
+                        //                            string queryString1 = "SELECT ER.[First Name], ER.[Last Name], ER.[Birth Date], E.[First Name] , E.[Last Name], E.[Job Title]FROM [CRONUS Sverige AB$Employee] E JOIN[CRONUS Sverige AB$Employee Relative] ER ON ER.[Employee No_] = E.No_;
+                        //             break;
+                        //            case "Information about Employees that have been absent due to sickness in the year of 2004";
+                        //            //
+                        //             break;
+                        //            case "First name of employee that has been absent the most";
+                        //                //
+                        //                break;
+                        //            case "ALL KEYS";
+                        //                //
+                        //                break;
+                        //            case "ALL INDEXES";
+                        //                //
+                        //                break;
+
+                        //            case "ALL TABLE_CONSTRAINTS";
+                        //                //
+                        //                break;
+                        //            case "ALL TABLES IN THE DATABASE 1";
+                        //                //
+                        //                break;
+                        //            case "ALL TABLES IN THE DATABASE 2";
+                        //                //
+                        //                break;
+                        //            case "ALL COLUMNS OF THE EMPLOYEE TABLE 1";
+                        //                //
+                        //                break;
+                        //            case "ALL COLUMNS OF THE EMPLOYEE TABLE 2";
+                        //                //
+                        //                break;
+
+
+                        //        }
                     }
-
-                //    void SendTable()
-                //{
-                //    string response = "";
-                //    while (response != null)
-                //    {
-                //        response = Console.ReadLine();
-                //        switch(response)
-                //        {
-                //            case "Contents and metadata for the Employee tables and related tables";
-                //                            string queryString = "";
-                //            break;
-                //            case "Information about Employees and their relatives";
-                //                            string queryString1 = "SELECT ER.[First Name], ER.[Last Name], ER.[Birth Date], E.[First Name] , E.[Last Name], E.[Job Title]FROM [CRONUS Sverige AB$Employee] E JOIN[CRONUS Sverige AB$Employee Relative] ER ON ER.[Employee No_] = E.No_;
-                //             break;
-                //            case "Information about Employees that have been absent due to sickness in the year of 2004";
-                //            //
-                //             break;
-                //            case "First name of employee that has been absent the most";
-                //                //
-                //                break;
-                //            case "ALL KEYS";
-                //                //
-                //                break;
-                //            case "ALL INDEXES";
-                //                //
-                //                break;
-
-                //            case "ALL TABLE_CONSTRAINTS";
-                //                //
-                //                break;
-                //            case "ALL TABLES IN THE DATABASE 1";
-                //                //
-                //                break;
-                //            case "ALL TABLES IN THE DATABASE 2";
-                //                //
-                //                break;
-                //            case "ALL COLUMNS OF THE EMPLOYEE TABLE 1";
-                //                //
-                //                break;
-                //            case "ALL COLUMNS OF THE EMPLOYEE TABLE 2";
-                //                //
-                //                break;
-
-
-                //        }
-            }
 }
 }
 }
---
+++ HyggeAssignment5Solution/HyggeAssignment5/ParamArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HyggeAssignment5
+{
+    public class ParamArgs
+    {
+
+        public ParamArgs(string paramID, object val)
+        { ParamID = paramID; Value = val; }
+
+        public string ParamID { get; set; } 
+        public object Value { get; set; }   
+
+        }
+}
