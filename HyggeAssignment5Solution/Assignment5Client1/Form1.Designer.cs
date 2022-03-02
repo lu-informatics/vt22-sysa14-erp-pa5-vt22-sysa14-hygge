b06961bd7c3d39441e5b2d76ba740421bd1235b6
ma7762la-s <majjalarsen@hotmail.com>
2022-03-02 15:00:52 +0000

    new

--- HyggeAssignment5Solution/Assignment5Client1/Form1.Designer.cs
+++ HyggeAssignment5Solution/Assignment5Client1/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBoxValue = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxValue
+            // 
+            this.comboBoxValue.FormattingEnabled = true;
+            this.comboBoxValue.Location = new System.Drawing.Point(75, 67);
+            this.comboBoxValue.Name = "comboBoxValue";
+            this.comboBoxValue.Size = new System.Drawing.Size(244, 23);
+            this.comboBoxValue.TabIndex = 0;
+            this.comboBoxValue.SelectedIndexChanged += new System.EventHandler(this.comboBoxValue_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(75, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(452, 247);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select a value:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxValue);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxValue;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- HyggeAssignment5Solution/Assignment5Client1/Form1.cs
+++ HyggeAssignment5Solution/Assignment5Client1/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Assignment5Client1
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form //Döpa om
     {
         static HyggeServiceSoapClient.EndpointConfiguration config = HyggeServiceSoapClient.EndpointConfiguration.HyggeServiceSoap;
         HyggeServiceSoapClient proxy = new HyggeServiceSoapClient(config);
@@ -20,5 +20,28 @@ namespace Assignment5Client1
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxValue.Items.Add("All tables in the database 1");
+        }
+
+        private void comboBoxValue_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+
+            ArrayOfXElement ds = null;
+
+            switch (cb.Text)
+            {
+                case "All tables in the database 1":
+                    ds = proxy.Table("All tables in the database 1");
+                    break;
+            }
+            dataGridView1.DataSource = ToDataSet
     }
+        public DataSet ToDataSet(ArrayOfXElement arrayOfXElement)
+        {
+
+        }
+}
 }
